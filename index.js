08b360861c484a507ae16c9cafdc501f2757d0a8
abdennour <mail@abdennoor.com>
2016-11-11 22:39:00 +0300

    done

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const data = {
  reset: "\x1b[0m",
  reverse: "\x1b[7m"
 };
+console.old_warn =  console.warn ;
 
 console.warn = function() {
- console.log(data.yellowBg + data.whiteFg, data.icon, data.reset, data.yellowFg, ...arguments, data.reset);
+ console.old_warn(data.yellowBg + data.whiteFg, data.icon, data.reset, data.yellowFg, ...arguments, data.reset);
 };
 module.exports = console.warn ;
